fdebea7c368e9ab2450dde2c808ea2b800bc9726
unknown <sseenivasan89@gmail.com>
2022-11-11 18:04:47 +0530

    initial

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Resource
+    https://www.callicoder.com/spring-boot-file-upload-download-rest-api-example/
+## Setup 
+  - clone the repo
+  - change the file upload path in src/main/resources/application.properties
+    file.upload-dir=./../folderName
+
+## Run the project
+  ```sh
+mvn spring-boot:run
+```
+## After Run
+  ```sh
+go to [please go this url](http://localhost:8080)
+
+```
+
+ **Now the application is ready to use**
+
---
+++ src/test/java/com/example/filedemo/FileDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.filedemo;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileDemoApplicationTests {
+
+	public static void main(String[] args) {
+        SpringApplication.run(FileDemoApplication.class, args);
+    }
+
+}
